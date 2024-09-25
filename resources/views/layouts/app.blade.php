8d9c4fb68354f5918812afa0808221e072d5155d
kris-15 <christkuzanuka@gmail.com>
2024-09-25 08:29:36 +0100

    Design background-image et logo epst

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,65 @@
             @endisset
 
             <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="flex h-screen bg-gray-100 relative overflow-hidden">
+                <div class="absolute inset-0 overflow-hidden">
+                    <div class="slideshow-container">
+                        <div class="slide bg-cover bg-center" style='background-image: url({{ asset("img/epst_logo.jpg")}}); background-size: cover; background-position: center; max-height: 80vh;'></div>
+                        <div class="slide bg-cover bg-center" style='background-image: url({{ asset("img/logo_epst.jpg")}}); background-size: cover; background-position: center; max-height: 100%;'></div>
+                        <div class="slide bg-cover bg-center" style='background-image: url({{ asset("img/logo_edunc.png")}}); background-size: cover; background-position: center; max-height: 80vh; max-width:100%'></div>
+                    </div>
+                    <div class="overlay bg-black opacity-40"></div>
+                </div>
+                <div class="flex-1 p-6 z-10">
+                    <main>
+                        {{ $slot }}
+                    </main>
+                </div>
+            </div>
         </div>
     </body>
 </html>
+<script>
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = (i === index) ? 'block' : 'none';
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    }
+
+    // Initial display of the first slide
+    showSlide(currentSlide);
+    // Change slide every 5 seconds
+    setInterval(nextSlide, 5000);
+</script>
+
+<style>
+    .slideshow-container {
+        position: relative;
+        height: 100%;
+    }
+
+    .slide {
+        display: none;
+        background-size: cover;
+        background-position: center;
+        height: 100%;
+        width: 100%;
+    }
+
+    .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+</style>
